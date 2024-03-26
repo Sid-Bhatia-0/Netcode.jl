38f9f2389a3a56839610ee3718313725a2c74ebf
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-03-25 19:38:02 -0500

    add simulate.jl to netcode dir

---
+++ netcode/simulate.jl
@@ -0,0 +1,24 @@
+import Netcode
+
+if length(ARGS) == 1
+    if ARGS[1] == "--app_server"
+        @info "Running as app_server" Netcode.APP_SERVER_ADDRESS Netcode.AUTH_SERVER_ADDRESS
+
+        Netcode.start_app_server(Netcode.APP_SERVER_ADDRESS, Netcode.ROOM_SIZE, Netcode.USED_CONNECT_TOKEN_HISTORY_SIZE)
+
+    elseif ARGS[1] == "--auth_server"
+        @info "Running as auth_server" Netcode.APP_SERVER_ADDRESS Netcode.AUTH_SERVER_ADDRESS
+
+        Netcode.start_auth_server(Netcode.AUTH_SERVER_ADDRESS)
+
+    elseif ARGS[1] == "--client"
+        @info "Running as client" Netcode.APP_SERVER_ADDRESS Netcode.AUTH_SERVER_ADDRESS
+
+        Netcode.start_client(Netcode.AUTH_SERVER_ADDRESS, Netcode.CLIENT_USERNAME, Netcode.CLIENT_PASSWORD)
+
+    else
+        error("Invalid command line argument $(ARGS[1])")
+    end
+elseif length(ARGS) > 1
+    error("This script accepts at most one command line flag")
+end
