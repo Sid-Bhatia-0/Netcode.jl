b589ddbe49f4c3b2ebb82b5a6f9285ebd38ee311
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-03-25 19:07:25 -0500

    clean up runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,2 @@
-import SimpleTopDownShooter as STDS
+import Netcode
 import Test
-
-Test.@testset "SimpleTopDownShooter.jl" begin
-    Test.@testset "integer_sqrt" begin
-        for i in 0:1000
-            Test.@test STDS.integer_sqrt(i) == isqrt(i)
-        end
-    end
-end
