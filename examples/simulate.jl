fd26b0b871e743ac0086322a1549d9ce25e8bb7b
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-07-05 14:36:43 -0500

    use create_logger to create and use a composite logger

--- examples/simulate.jl
+++ examples/simulate.jl
@@ -6,19 +6,19 @@ if length(ARGS) == 0
     @info "Empty run"
 elseif length(ARGS) == 1
     if ARGS[1] == "--app_server"
-        Logging.global_logger(LE.FileLogger("app_server.log"))
+        Logging.global_logger(Netcode.create_logger("app_server"))
 
         @info "Running as app_server"
 
         Netcode.test_app_server()
     elseif ARGS[1] == "--auth_server"
-        Logging.global_logger(LE.FileLogger("auth_server.log"))
+        Logging.global_logger(Netcode.create_logger("auth_server"))
 
         @info "Running as auth_server"
 
         Netcode.test_auth_server()
     elseif ARGS[1] == "--client"
-        Logging.global_logger(LE.FileLogger("client.log"))
+        Logging.global_logger(Netcode.create_logger("client"))
 
         @info "Running as client"
 
