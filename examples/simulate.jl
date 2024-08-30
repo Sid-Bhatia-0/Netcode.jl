15127ffd6fa5a0ec0c4da4e402f7294dc6679088
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-08-29 22:58:41 -0500

    trim simulate.jl

--- examples/simulate.jl
+++ examples/simulate.jl
@@ -1,31 +1,15 @@
-import Logging
-import LoggingExtras as LE
 import Netcode
 
-if length(ARGS) == 0
-    @info "Empty run"
-elseif length(ARGS) == 1
+if length(ARGS) == 1
     if ARGS[1] == "--app_server"
-        Logging.global_logger(Netcode.create_logger("app_server", [Main, Netcode]))
-
-        @info "Running as app_server"
-
         Netcode.test_app_server()
     elseif ARGS[1] == "--auth_server"
-        Logging.global_logger(Netcode.create_logger("auth_server", [Main, Netcode]))
-
-        @info "Running as auth_server"
-
         Netcode.test_auth_server()
     elseif ARGS[1] == "--client"
-        Logging.global_logger(Netcode.create_logger("client", [Main, Netcode]))
-
-        @info "Running as client"
-
         Netcode.test_client()
     else
         error("Unknown command line argument $(ARGS[1])")
     end
-else
+elseif length(ARGS) > 1
     error("Invalid command line argument structure")
 end
