86ef000f8399016b1ed7c6252ab233f717254257
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2024-08-31 16:56:09 -0500

    set game_start_time field to be 0 in constructor for GameState

--- src/miscellaneous.jl
+++ src/miscellaneous.jl
@@ -42,7 +42,7 @@ FrameDebugInfo() = FrameDebugInfo(0, 0, 0, 0, 0, 0, 0, [], [])
 
 function GameState(target_frame_rate, total_frames)
     target_ns_per_frame = 1_000_000_000 ÷ target_frame_rate
-    return GameState(time_ns(), 1, 0, target_frame_rate, target_ns_per_frame, total_frames)
+    return GameState(0, 1, 0, target_frame_rate, target_ns_per_frame, total_frames)
 end
 
 function NetcodeAddress(address::Union{Sockets.InetAddr{Sockets.IPv4}, Sockets.InetAddr{Sockets.IPv6}})
