1c92b11fbb674689981d84efca0e122b769656d3
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2025-01-18 21:30:03 -0600

    fix method generate_packet_prefix

--- src/miscellaneous.jl
+++ src/miscellaneous.jl
@@ -373,7 +373,7 @@ get_packet_prefix(packet_data::Vector{UInt8})::TYPE_OF_PACKET_PREFIX = first(pac
 
 get_packet_type(packet_prefix::TYPE_OF_PACKET_PREFIX)::TYPE_OF_PACKET_TYPE = packet_prefix & 0xf
 
-generate_packet_prefix(packet_type::TYPE_OF_PACKET_TYPE, packet_sequence_number) = packet_type | convert(TYPE_OF_PACKET_PREFIX, get_netcode_serialized_size(CompactUnsignedInteger(packet_sequence_number)))
+generate_packet_prefix(packet_type::TYPE_OF_PACKET_TYPE, packet_sequence_number) = packet_type | (convert(TYPE_OF_PACKET_PREFIX, get_netcode_serialized_size(CompactUnsignedInteger(packet_sequence_number))) << 4)
 
 function create_logger(name, modules)
     return LE.EarlyFilteredLogger(
